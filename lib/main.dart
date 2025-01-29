0d538375af159c215aac6e5cdaf0afcf97bd584f
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2025-01-29 15:02:10 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+void main() {
+  runApp(const MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: VideoScreen(),
+    );
+  }
+}
 class VideoScreen extends StatefulWidget {
   const VideoScreen({super.key});
 
